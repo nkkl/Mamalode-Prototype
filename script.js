03cca67e1e9d0f20b669f489332c2834506fc816
Nikki Lee <nikki@nicoleblee.com>
2011-11-21 18:01:03 -0800

    Hella styling, and some js that needs debugging

--- script.js
+++ script.js
@@ -5,22 +5,49 @@ $(document).ready(function() {
 	// catch swipes and "turn" the pages
 	$(".page").wipetouch(
 	{
-		tapToClick: true,
-
 		// wipe from right to left
 		// (turn to the NEXT page)
-		wipeLeft: function(result) {
-			//
+		wipeLeft: function(result) {	
+			var page_id = parseInt($(".active").attr("id"));
+			var new_page_id;
+
+			// if we are on the last page, don't turn
+			if ( page_id == 3 ) {
+				alert("can't turn any farther");
+			} else {
+				new_page_id = page_id + 1;
+				new_page_id = "#" + new_page_id;
+				
+				// TODO: fix this shit
+				// PROBLEM: can't call standard jquery within wipetouch functions?
+				// make the current div inactive
+				(".active").hide();
+				(".active").removeClass("active");
+				
+				// make the "next page" the active div
+				(new_page_id).show();
+				(new_page_id).addClass("active");
+			};
 		},
+
 		// wipe from left to right
 		// (turn to the PREVIOUS page)
 		wipeRight: function(result) {
-			// alert(result.x);
+			var page_id = parseInt($(".active").attr("id"));
+			var new_page_id;
+
+			// if we are on the first page, don't turn
+			if (page_id == 1 ) {
+				alert("can't turn any earlier");
+			} else {
+				new_page_id = page_id - 1;
+				alert("right " + new_page_id);
+			}
 		},
 	});
 
 	$(".page").click(function() {
 		var page_id = $(this).attr("id");
-		alert(page_id);
+		alert("click " + page_id);
 	});
 });
