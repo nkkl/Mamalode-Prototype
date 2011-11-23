900549cbb7f1af7e5ac73c2221bd8960563756b6
Nikki Lee <nikki@nicoleblee.com>
2011-11-22 18:47:36 -0800

    Rewrote js to follow jquery mobile conventions

--- script.js
+++ script.js
@@ -3,49 +3,39 @@
 // wait until everything is loaded before running any js
 $(document).ready(function() {	
 	// catch swipes and "turn" the pages
-	
-	// wipe from right to left
-	// (turn to the NEXT page)
-	$(".page").swipeleft(function() {
-		var page_id = parseInt($(".active").attr("id"));
-		var new_page_id;
+	$(".page").live("swipeleft swiperight", function(event) {
+		if (event.type == "swipeleft") {
+			var page_id = parseInt($(".active").attr("id"));
+			var new_page_id;
 
-		// if we are on the last page, don't turn
-		if ( page_id == 3 ) {
-			alert("can't turn any later");
-		} else {
-			new_page_id = page_id + 1;
-			new_page_id = "#" + new_page_id;
-			
-			$(new_page_id).addClass("active");
-			$(new_page_id).show();
-			$("#" + page_id).removeClass("active");
-			$("#" + page_id).hide();
+			// if we are on the last page, don't turn
+			if ( page_id == 3 ) {
+				alert("can't turn any later");
+			} else {
+				new_page_id = page_id + 1;
+				new_page_id = "#" + new_page_id;
+				
+				$(new_page_id).addClass("active");
+				$("#" + page_id).removeClass("active");
+				$.mobile.changePage($(new_page_id), { transition: "slideleft" });
+			}
+		}
+		if (event.type == "swiperight") {
+			var page_id = parseInt($(".active").attr("id"));
+			var new_page_id;
+
+			// if we are on the first page, don't turn
+			if (page_id == 1 ) {
+				alert("can't turn any earlier");
+			} else {
+				new_page_id = page_id - 1;
+				new_page_id = "#" + new_page_id;
+
+				$(new_page_id).addClass("active");
+				$("#" + page_id).removeClass("active");
+				$.mobile.changePage($(new_page_id), { transition: "slideright" });
+			}
 		}
 	});
 
-	// wipe from left to right
-	// (turn to the PREVIOUS page)
-	$(".page").swiperight(function() {
-		var page_id = parseInt($(".active").attr("id"));
-		var new_page_id;
-
-		// if we are on the first page, don't turn
-		if (page_id == 1 ) {
-			alert("can't turn any earlier");
-		} else {
-			new_page_id = page_id - 1;
-			new_page_id = "#" + new_page_id;
-
-			$(new_page_id).addClass("active");
-			$(new_page_id).show();
-			$("#" + page_id).removeClass("active");
-			$("#" + page_id).hide();
-		}
-	});
-
-	$(".page").click(function() {
-		var page_id = $(this).attr("id");
-		alert("click " + page_id);
-	});
 });
