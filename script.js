500555119709e0c7278982f74a7e56090841638d
Nikki Lee <nikki@nicoleblee.com>
2011-11-22 16:35:57 -0800

    Page turning now works, js library switched to jquery mobile

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 // Main javascript file
 
 // wait until everything is loaded before running any js
-$(document).ready(function() {
+$(document).ready(function() {	
 	// catch swipes and "turn" the pages
-	$(".page").wipetouch(
-	{
-		// wipe from right to left
-		// (turn to the NEXT page)
-		wipeLeft: function(result) {	
-			var page_id = parseInt($(".active").attr("id"));
-			var new_page_id;
+	
+	// wipe from right to left
+	// (turn to the NEXT page)
+	$(".page").swipeleft(function() {
+		var page_id = parseInt($(".active").attr("id"));
+		var new_page_id;
 
-			// if we are on the last page, don't turn
-			if ( page_id == 3 ) {
-				alert("can't turn any farther");
-			} else {
-				new_page_id = page_id + 1;
-				new_page_id = "#" + new_page_id;
-				
-				// TODO: fix this shit
-				// PROBLEM: can't call standard jquery within wipetouch functions?
-				// make the current div inactive
-				(".active").hide();
-				(".active").removeClass("active");
-				
-				// make the "next page" the active div
-				(new_page_id).show();
-				(new_page_id).addClass("active");
-			};
-		},
+		// if we are on the last page, don't turn
+		if ( page_id == 3 ) {
+			alert("can't turn any later");
+		} else {
+			new_page_id = page_id + 1;
+			new_page_id = "#" + new_page_id;
+			
+			$(new_page_id).addClass("active");
+			$(new_page_id).show();
+			$("#" + page_id).removeClass("active");
+			$("#" + page_id).hide();
+		}
+	});
 
-		// wipe from left to right
-		// (turn to the PREVIOUS page)
-		wipeRight: function(result) {
-			var page_id = parseInt($(".active").attr("id"));
-			var new_page_id;
+	// wipe from left to right
+	// (turn to the PREVIOUS page)
+	$(".page").swiperight(function() {
+		var page_id = parseInt($(".active").attr("id"));
+		var new_page_id;
 
-			// if we are on the first page, don't turn
-			if (page_id == 1 ) {
-				alert("can't turn any earlier");
-			} else {
-				new_page_id = page_id - 1;
-				alert("right " + new_page_id);
-			}
-		},
+		// if we are on the first page, don't turn
+		if (page_id == 1 ) {
+			alert("can't turn any earlier");
+		} else {
+			new_page_id = page_id - 1;
+			new_page_id = "#" + new_page_id;
+
+			$(new_page_id).addClass("active");
+			$(new_page_id).show();
+			$("#" + page_id).removeClass("active");
+			$("#" + page_id).hide();
+		}
 	});
 
 	$(".page").click(function() {
