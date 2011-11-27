c7d98eb843aad1eb6d6352a8305102906bd433eb
Adam Rule <acrule@gmail.com>
2011-11-26 21:43:52 -0800

    Adam version 1 commit

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ $(document).ready(function() {
 	$(".page").live("swipeleft swiperight tap", function(event) {
 		// catch right-to-left swipes
 		if (event.type == "swipeleft") {
-			var page_id = parseInt($(this).attr("id"));
+			var page_str = $(this).attr("id");
+			page_str = page_str.replace("p","");
+			var page_id = parseInt(page_str);
 			var new_page_id;
 
 			// if we are on the last page, don't turn
-			if ( page_id == 3 ) {
+			if ( page_id == 44 ) {
 				alert("can't turn any later");
 			} else {
 				new_page_id = page_id + 1;
-				new_page_id = "#" + new_page_id;
+				new_page_id = "#p" + new_page_id;
 				
 				$(this).hide();
 				$(new_page_id).show("slide", { direction: "right", distance: "50px" }, 500);
@@ -23,15 +25,17 @@ $(document).ready(function() {
 
 		// catch left-to-right swipes
 		if (event.type == "swiperight") {
-			var page_id = parseInt($(this).attr("id"));
+			var page_str = $(this).attr("id");
+			page_str = page_str.replace("p","");
+			var page_id = parseInt(page_str);
 			var new_page_id;
 
 			// if we are on the first page, don't turn
-			if (page_id == 1 ) {
+			if (page_id == 0 ) {
 				alert("can't turn any earlier");
 			} else {
 				new_page_id = page_id - 1;
-				new_page_id = "#" + new_page_id;
+				new_page_id = "#p" + new_page_id;
 
 				$(this).hide();
 				$(new_page_id).show("slide", { direction: "left", distance: "50px" }, 500);
