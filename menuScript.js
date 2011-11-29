ccec9f7d038597d8c1b74be0e6b7fd43f5ac9b34
Asmi Joshi <asmij89@gmail.com>
2011-11-28 16:08:55 -0800

    Working on highlights

--- menuScript.js
+++ menuScript.js
@@ -1,3 +1,4 @@
+
 $(function() {		
 
 	/*
@@ -6,4 +7,23 @@ $(function() {
 	 */ 
 	$(".scrollable").scrollable({ vertical: true, mousewheel: true }).navigator({navi: "#toc-nav"});
 
+	$('.nav').click(function() {
+		
+		var tab = $(this);
+    	
+    	tab.parent().find('.nav').css('background-color','#40E0D0');
+    	
+    	$(this).parent.find($("#mamasays-nav").css('background', "#FFD700");
+    	$(this).parent.find($("#because-nav").css('background', "#ADD8E6");
+    	$(this).parent.find($("#expressions-nav").css('background', "#DC143C");
+    	$(this).parent.find($("#bits-nav").css('background', "#C0C");
+    	$(this).parent.find($("#fashion-nav").css('background', "#09F");
+    	$(this).parent.find($("#perspective-nav").css('background', "#40E0D0");
+    	$(this).parent.find($("#bookmarks-nav").css('background', rgb(100,100,100)); 
+    	
+    	tab.css('background',"#D87093");
+    	
+	});
+
 });
+
